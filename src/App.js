8b0833584d58019c5d648d182bc7f1476984c771
Tran Thanh Hoa <139173700+TranHoa21@users.noreply.github.com>
2024-06-03 01:58:19 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import { marked } from 'marked';
 import './App.css';
-import marked from 'marked';
 
-// Mã Markdown mặc định
-const initialMarkdown = `# Welcome to my React Markdown Previewer!
-
-## This is a sub-heading...
-### And here's some other cool stuff:
-
-Here's some code, \`<div></div>\`, between 2 backticks.
-
+const initialMarkdown = `
+# Heading 1
+## Heading 2
+[Link](https://example.com)
+Inline \`code\`
 \`\`\`
-// this is multi-line code:
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
+Code block
 \`\`\`
-
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
-
-There's also [links](https://www.freecodecamp.org), and
-> Block Quotes!
-
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
-
-1. And there are numbererd lists too.
-1. Use just 1s if you want!
-1. And last but not least, let's not forget embedded images:
-
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+- List item
+> Blockquote
+![Image](https://via.placeholder.com/150)
+**Bold text**
 `;
 
 function App() {
   const [markdown, setMarkdown] = useState(initialMarkdown);
 
-  const handleChange = (event) => {
-    setMarkdown(event.target.value);
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
   };
 
   useEffect(() => {
@@ -51,13 +28,25 @@ function App() {
   }, [markdown]);
 
   return (
-    <div className="App">
-      <textarea
-        id="editor"
-        value={markdown}
-        onChange={handleChange}
-      />
-      <div id="preview"></div>
+    <div className="App container">
+      <h1 className="text-center my-4">Markdown Previewer</h1>
+      <div className="row">
+        <div className="col-6">
+          <textarea
+            id="editor"
+            className="form-control"
+            value={markdown}
+            onChange={handleChange}
+            rows="15"
+          />
+        </div>
+        <div className="col-6">
+          <div
+            id="preview"
+            className="preview p-3 border"
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
